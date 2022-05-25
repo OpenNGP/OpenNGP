f9864e3d62f2dcd9edcda0eb42e379c787deaffe
fanxiaochen <fan.daybreak@gmail.com>
2022-05-25 23:57:29 +0800

    fixing unittest

--- ongp/app/main.cc
+++ ongp/app/main.cc
@@ -1,31 +1,31 @@
-#include <torch/torch.h>
-#include <iostream>
-
-#include "ongp/dataset/frame_dataset.h"
-#include "ongp/base/tensor.h"
-
-int main() {
-  std::vector<std::vector<float>> vec{
-    {1,2,3,4},
-    {1,2,3,4},
-    {1,2,3,4},
-    {1,2,3,4},
-  };
-
-  std::cout << ongp::Array2dToTensor(vec) << std::endl;
-
-  torch::Tensor tensor = torch::rand({4, 3});
-  std::cout << tensor[1][1] << std::endl;
-
-  std::string lego_test_dir = "/nerf/openNGP/data/lego/";
-  std::string lego_test_data = "transforms_test.json";
-
-  ongp::FrameDataset fd(lego_test_dir, lego_test_data);
-  std::cout << fd.size() << std::endl;
-  fd.get(10);
-//  for (size_t i = 0; i < fd.size(); ++ i) 
-//  {
-//    std::cout << fd.get(i).cam().pose().mat44() << std::endl;
-//  }
-
-}
+//#include <torch/torch.h>
+//#include <iostream>
+//
+//#include "ongp/dataset/frame_dataset.h"
+//#include "ongp/base/tensor.h"
+//
+//int main() {
+//  std::vector<std::vector<float>> vec{
+//    {1,2,3,4},
+//    {1,2,3,4},
+//    {1,2,3,4},
+//    {1,2,3,4},
+//  };
+//
+//  std::cout << ongp::Array2dToTensor(vec) << std::endl;
+//
+//  torch::Tensor tensor = torch::rand({4, 3});
+//  std::cout << tensor[1][1] << std::endl;
+//
+//  std::string lego_test_dir = "/nerf/openNGP/data/lego/";
+//  std::string lego_test_data = "transforms_test.json";
+//
+//  ongp::FrameDataset fd(lego_test_dir, lego_test_data);
+//  std::cout << fd.size() << std::endl;
+//  fd.get(10);
+////  for (size_t i = 0; i < fd.size(); ++ i) 
+////  {
+////    std::cout << fd.get(i).cam().pose().mat44() << std::endl;
+////  }
+//
+//}
--- test/cpp/test_ray_tracing.cc
+++ test/cpp/test_ray_tracing.cc
@@ -3,6 +3,8 @@
 #include <torch/torch.h>
 
 #include "ongp/base/sphere.h"
+#include "ongp/base/scene.h"
+#include "ongp/base/tensor.h"
 
 
 TEST(RAYTRACING, RENDER) {
@@ -13,34 +15,34 @@ TEST(RAYTRACING, RENDER) {
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // World
-    std::vector<std::shared_ptr<Sphere>> spheres;
-    spheres.push_back(make_shared<sphere>(torch::Tensor({0,0,-1}), 0.5));
-    spheres.push_back(make_shared<sphere>(torch::Tensor({0,-100.5,-1}), 100));
+    ongp::Scene scene;
+    scene.Add(std::make_shared<ongp::Sphere>(ongp::Array1dToTensor<float>({0,0,-1}), 0.5));
+    scene.Add(std::make_shared<ongp::Sphere>(ongp::Array1dToTensor<float>({0,-100.5,-1}), 100));
 
-    // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-
-    // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::cerr << "\nDone.\n";
+//    // Camera
+//    auto viewport_height = 2.0;
+//    auto viewport_width = aspect_ratio * viewport_height;
+//    auto focal_length = 1.0;
+//
+//    auto origin = point3(0, 0, 0);
+//    auto horizontal = vec3(viewport_width, 0, 0);
+//    auto vertical = vec3(0, viewport_height, 0);
+//    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+//
+//    // Render
+//    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+//
+//    for (int j = image_height-1; j >= 0; --j) {
+//        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+//        for (int i = 0; i < image_width; ++i) {
+//            auto u = double(i) / (image_width-1);
+//            auto v = double(j) / (image_height-1);
+//            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+//            color pixel_color = ray_color(r, world);
+//            write_color(std::cout, pixel_color);
+//        }
+//    }
+//
+//    std::cerr << "\nDone.\n";
 
 }
